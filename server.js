c10da4852195aac63c5ac13582cd67bfe30ed34e
Jared Scott <104541761+JaredScott66@users.noreply.github.com>
2025-03-15 17:13:35 -0500

    added swagger, added PUT POST DELETE methods

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const routes = require("./routes");
 const mongodb = require('./db/database.js');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origen, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', routes);
 
 mongodb.initDb((err) => {
